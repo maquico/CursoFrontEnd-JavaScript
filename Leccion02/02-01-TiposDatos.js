967373ecdeb335d83b7471253cfdc23e8a06a810
angel <angelgmorenor@gmail.com>
2023-08-05 15:56:49 -0400

    02-01-tipos de datos terminado

--- Leccion02/02-01-TiposDatos.js
+++ Leccion02/02-01-TiposDatos.js
@@ -5,7 +5,10 @@ de datos en javascript
 
 //Tipo de dato string
 var nombre = "Carlos";
-console.log(nombre);
+console.log(typeof nombre);
+
+nombre = 10;
+console.log(typeof nombre);
 
 //Tipo de dato numerico
 var numero = 1000;
@@ -17,4 +20,45 @@ var objeto = {
     apellido: "Perez",
     telefono: "123456789"
 };
-console.log(objeto);
+console.log(typeof objeto);
+
+//Tipo de dato boolean (true, false)
+var bandera = false;
+console.log(typeof bandera);
+
+//Tipo de dato function
+function miFuncion(){}
+console.log(typeof miFuncion)
+
+//Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+//Tipo clase (es una function)
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona)
+
+//Tipo undefined
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(x);
+
+//null = ausencia de valor
+var y = null;
+console.log(typeof y);
+
+//arrays
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
