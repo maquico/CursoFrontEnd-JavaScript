02cb5c301c1b160f5124e98b71859eadfbd29f10
angel <angelgmorenor@gmail.com>
2023-08-07 14:43:47 -0400

    02-01-Concatenacion de cadenas

--- Leccion02/02-01-TiposDatos.js
+++ Leccion02/02-01-TiposDatos.js
@@ -1,64 +1,92 @@
-/*
-Ejemplos de tipos
-de datos en javascript
-*/
+var nombre = 'Juan';
+var apellido = 'Perez';
 
-//Tipo de dato string
-var nombre = "Carlos";
-console.log(typeof nombre);
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
 
-nombre = 10;
-console.log(typeof nombre);
+var nombreCompleto2 = 'Carlos' + ' ' + 'Lara';
+console.log(nombreCompleto2);
 
-//Tipo de dato numerico
-var numero = 1000;
-console.log(numero);
-
-//Tipo de dato object
-var objeto = {
-    nombre: "Juan",
-    apellido: "Perez",
-    telefono: "123456789"
-};
-console.log(typeof objeto);
-
-//Tipo de dato boolean (true, false)
-var bandera = false;
-console.log(typeof bandera);
-
-//Tipo de dato function
-function miFuncion(){}
-console.log(typeof miFuncion)
-
-//Tipo de dato Symbol
-var simbolo = Symbol("mi simbolo");
-console.log(typeof simbolo);
-
-//Tipo clase (es una function)
-class Persona{
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-console.log(typeof Persona)
-
-//Tipo undefined
-var x;
-console.log(typeof x);
-
-x = undefined;
+var x = nombre + 219;
 console.log(x);
 
-//null = ausencia de valor
-var y = null;
-console.log(typeof y);
+x = nombre + (2+4);
+console.log(x);
 
-//arrays
-var autos = ['BMW', 'Audi', 'Volvo'];
-console.log(autos);
-console.log(typeof autos);
+x = 2 + 4 + nombre;
+console.log(x);
 
-var z = '';
-console.log(z);
-console.log(typeof z);
+
+
+
+
+
+
+
+
+
+
+// /*
+// Ejemplos de tipos
+// de datos en javascript
+// */
+
+// //Tipo de dato string
+// var nombre = "Carlos";
+// console.log(typeof nombre);
+
+// nombre = 10;
+// console.log(typeof nombre);
+
+// //Tipo de dato numerico
+// var numero = 1000;
+// console.log(numero);
+
+// //Tipo de dato object
+// var objeto = {
+//     nombre: "Juan",
+//     apellido: "Perez",
+//     telefono: "123456789"
+// };
+// console.log(typeof objeto);
+
+// //Tipo de dato boolean (true, false)
+// var bandera = false;
+// console.log(typeof bandera);
+
+// //Tipo de dato function
+// function miFuncion(){}
+// console.log(typeof miFuncion)
+
+// //Tipo de dato Symbol
+// var simbolo = Symbol("mi simbolo");
+// console.log(typeof simbolo);
+
+// //Tipo clase (es una function)
+// class Persona{
+//     constructor(nombre, apellido){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//     }
+// }
+// console.log(typeof Persona)
+
+// //Tipo undefined
+// var x;
+// console.log(typeof x);
+
+// x = undefined;
+// console.log(x);
+
+// //null = ausencia de valor
+// var y = null;
+// console.log(typeof y);
+
+// //arrays
+// var autos = ['BMW', 'Audi', 'Volvo'];
+// console.log(autos);
+// console.log(typeof autos);
+
+// var z = '';
+// console.log(z);
+// console.log(typeof z);
